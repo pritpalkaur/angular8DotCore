411efbfc66dc0fac2d1b166ea25effb266c050db
phunjan <pritpalkaur05@gmail.com>
2021-08-20 11:24:56 +0800

    customer class is required for this project

---
+++ PaymentAPI/PaymentAPI/Controllers/CustomerController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+
+namespace PaymentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    { 
+        private readonly PaymentDetailContext _context;
+
+        public CustomerController(PaymentDetailContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PaymentDetail
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetPaymentDetails()
+        {
+            return await _context.Customer.ToListAsync();
+        }
+
+        // GET: api/PaymentDetail/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetPaymentDetail(int id)
+        {
+            var Customer = await _context.Customer.FindAsync(id);
+
+            if (Customer == null)
+            {
+                return NotFound();
+            }
+
+            return Customer;
+        }
+
+        // PUT: api/PaymentDetail/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentDetail(int id, Customer customerDetail)
+        {
+            if (id != customerDetail.CustomerId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customerDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PaymentDetail
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Customer>> PostPaymentDetail(Customer customerDetail)
+        {
+            _context.Customer.Add(customerDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaymentDetail", new { id = customerDetail.CustomerId }, customerDetail);
+        }
+
+        // DELETE: api/PaymentDetail/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePaymentDetail(int id)
+        {
+            var customerDetail = await _context.Customer.FindAsync(id);
+            if (customerDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customer.Remove(customerDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customer.Any(e => e.CustomerId == id);
+        }
+    }
+}
+
